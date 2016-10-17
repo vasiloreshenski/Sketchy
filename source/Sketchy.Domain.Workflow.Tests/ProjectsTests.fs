0e632e5f469c947c2e9988bef090b08814134c0b
vasiloreshenski <vasil.oreshenski@gmail.com>
2016-10-17 02:10:48 +0200

    added test for create project with too short name

--- source/Sketchy.Domain.Workflow.Tests/ProjectsTests.fs
+++ source/Sketchy.Domain.Workflow.Tests/ProjectsTests.fs
@@ -1,17 +1,23 @@
 ﻿namespace Sketchy.Domain.Workflow.Tests
 
-module ProjectsTests =
+module ProjectsTests = 
     open Xunit
-
     open Sketchy.Domain.Workflow
-
+    
     [<Fact>]
     let ``Create project when name is valid should succeed``() = 
-        let existingProjectNames = [""]
+        let existingProjectNames = [ "" ]
         let project = Projects.Create existingProjectNames "project Name"
         ()
-
+    
     [<Fact>]
     let ``Create project when has forbidden charachter should raise exception``() = 
-        let _ = Assert.Throws(typedefof<Projects.InvalidCharachtersPorjectNameException>, (fun() ->  ignore <| Projects.Create [""] "~!@EDS"))
+        let action = (fun () -> ignore <| Projects.Create [ "" ] "~!@EDS")
+        let _ = Assert.Throws(typedefof<Projects.InvalidCharachtersPorjectNameException>, action)
+        ()
+    
+    [<Fact>]
+    let ``Create project with too short name should raise exception``() = 
+        let action = (fun () -> Projects.Create [ "" ] "a" |> ignore)
+        let _ = Assert.Throws(typedefof<Projects.InvalidLengthProjectNameException>, action)
         ()
