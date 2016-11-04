d88c658594892ea9a9723a308de654b76d4bcd53
vasiloreshenksi <vasil.oreshenski@gmail.com>
2016-11-04 21:39:35 +0200

    Validation and Core logic for Project are now in separated files

---
+++ source/Sketchy.Domain.Project/Api.fs
@@ -0,0 +1,20 @@
+﻿namespace Sketchy.Domain.Project
+
+/// Exposes the public functionlity from related to projects, workflows and shapes
+/// Provides capabilities to manupulate and create projects, workflows and shapes
+module Api = 
+    open Sketchy.Domain.Shared.CommonTypes
+
+    /// Creates a new project
+    let CreateProject(name : string) = ()
+
+    /// Renames the project by given identity and name
+    /// If no such project identity exception is raised
+    let RenameProject(projectIdentity: Identity) (newName: string) = ()
+
+    /// Deletes the project by given identity
+    let DeleteProject(projectIdentity: Identity) = ()
+
+    /// Restores a deleted project
+    /// If the project is not in deleted state, this method has no effect
+    let RestoreProject(projectIdentity: Identity) = ()
---
+++ source/Sketchy.Domain.Project/Core.fs
@@ -0,0 +1,38 @@
+﻿namespace Sketchy.Domain.Project.Core
+
+module internal Projects = 
+    open Sketchy.Common.Result
+    open Sketchy.Domain.Shared.CommonTypes
+    open Sketchy.Domain.Project.Validation
+    open Sketchy.Domain.Workflow.Workflows
+    
+    /// The project is main container for workflows
+    /// Project is describing by identity and name
+    type Project = 
+        { Identity : Identity
+          Name : string
+          Workflows : Workflow list }
+
+    /// Creates new project with empty identity
+    let CreateWithoutIdentity name workflows = 
+        { Identity = Identity.Empty
+          Name = name
+          Workflows = workflows }
+    
+    /// Creates project by given name and workflows
+    /// The result is an option type which fails if the name of the project is not valid
+    let CreateWithWorkflows (listOfProjectNames : string list) name (workflows : Workflow list) = 
+        let nameValidationResult = ProjectValidation.ValidateProjectName name listOfProjectNames
+        match nameValidationResult with
+        | ProjectValidation.Valid -> Value(CreateWithoutIdentity name workflows)
+        | error -> Error [ error ]
+    
+    /// Creates project by given name
+    /// The result is an option type which fails if the name of the project is not valid
+    let Create (listOfProjectNames : string list) name = MaybeResult { let! project = CreateWithWorkflows listOfProjectNames name Workflow.EmptyList
+                                                                       return project }
+    
+    /// Renames the specified project
+    /// The result is an option type which fails if the name of the project is not valid
+    let Rename (project : Project) (listOfProjectNames : string list) (name : string) = MaybeResult { let! renamed = Create listOfProjectNames name
+                                                                                                      return renamed }
